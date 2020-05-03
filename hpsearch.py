b7e983b7f68672c266fb20c31dc2b07332c08fa9
awurst3 <awurst@stevens.edu>
2020-05-02 22:52:03 -0400

    Add option to load best weights

--- hpsearch.py
+++ hpsearch.py
@@ -24,9 +24,18 @@ def get_test_sequences(model_dir):
     y_test = np.load(model_dir + 'y_test.npy')
     return x_test, y_test
 
-def get_weights_file(model_dir, num_epochs):
+def get_weights_last(model_dir, num_epochs):
     '''Get the name of the file that contains the model weights (based on the last training epoch)'''
     
     pattern = 'ep'+str(num_epochs)+'[\d\w._-]*.hdf5'
     matches = [re.search(pattern, file) for file in os.listdir(model_dir)]
-    return [i.group(0) for i in matches if i is not None][0]
+    return [i.group(0) for i in matches if i is not None][0]  # Only the last epoch will match, others are None
+
+def get_weights_best(model_dir, num_epochs):
+    '''Getthe name of the file containing the model weights (based on the best validation accuracy)'''
+
+    pattern = 'val_accuracy(0.\d\d)'
+    files = os.listdir(model_dir)
+    accuracies = [re.search(pattern, file) for file in files]
+    accuracies = [float(acc.group(1)) for acc in accuracies if acc is not None]  # For each match, get the accuracy
+    return [file for file in files if str(sorted(accuracies)[-1]) in file][-1]
